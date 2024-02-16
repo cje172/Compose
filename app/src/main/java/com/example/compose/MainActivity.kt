6a360e79ddbe0efb273f4b746bb236a061f9f1e3
최지은 <cje172@naver.com>
2024-02-16 17:23:16 +0900

    Jetpack Compose Basics
    - Create Compose Project
    - Common Compose UI Components
    - Compose functions (Composables)

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -3,33 +3,48 @@ package com.example.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.compose.ui.theme.ComposeTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Greeting("Jieun")
+//            Button(onClick = { println("Click") }, content = {
+//                Text(text = "Button")
+//            })
+//            Icon(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = null, tint = Color.Black)
+//            Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = null)
+//            TextField(value = "", onValueChange = {}, label = {
+//                Text(text = "Label")
+//            })
+//            FloatingActionButton(onClick = { /*TODO*/ }) {
+//                Icon(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = null)
+//            }
+//            ExtendedFloatingActionButton(
+//                text = {
+//                    Text(text = "Add")
+//                },
+//                onClick = { /*TODO*/ },
+//                icon = {
+//                    Icon(
+//                        painter = painterResource(id = R.drawable.ic_launcher_foreground),
+//                        contentDescription = null
+//                    )
+//                },
+//                backgroundColor = Color.White
+//            )
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeTheme {
-        Greeting("Android")
-    }
+    Text(text = "Welcome $name", color = Color.Blue, fontSize = 22.sp, fontWeight = FontWeight.Bold)
 }
