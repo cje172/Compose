1e0155ff01e570f05e67a3d34c8443d5ae8d14b0
최지은 <cje172@naver.com>
2024-02-17 17:12:01 +0900

    Jetpack Compose Layouts

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -4,47 +4,58 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Jieun")
-//            Button(onClick = { println("Click") }, content = {
-//                Text(text = "Button")
-//            })
-//            Icon(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = null, tint = Color.Black)
-//            Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = null)
-//            TextField(value = "", onValueChange = {}, label = {
-//                Text(text = "Label")
-//            })
-//            FloatingActionButton(onClick = { /*TODO*/ }) {
-//                Icon(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = null)
-//            }
-//            ExtendedFloatingActionButton(
-//                text = {
-//                    Text(text = "Add")
-//                },
-//                onClick = { /*TODO*/ },
-//                icon = {
-//                    Icon(
-//                        painter = painterResource(id = R.drawable.ic_launcher_foreground),
-//                        contentDescription = null
-//                    )
-//                },
-//                backgroundColor = Color.White
-//            )
+            ComposeTheme {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.CenterStart) {
+                    Text(text = "Hello", modifier = Modifier.align(Alignment.TopStart))
+                    Text(text = "Hello", modifier = Modifier.align(Alignment.TopCenter))
+                    Text(text = "Hello", modifier = Modifier.align(Alignment.TopEnd))
+                    Text(text = "Hello", modifier = Modifier.align(Alignment.CenterStart))
+                    Text(text = "Hello", modifier = Modifier.align(Alignment.Center))
+                    Text(text = "Hello", modifier = Modifier.align(Alignment.CenterEnd))
+                    Text(text = "Hello", modifier = Modifier.align(Alignment.BottomStart))
+                    Text(text = "Hello", modifier = Modifier.align(Alignment.BottomCenter))
+                    Text(text = "Hello", modifier = Modifier.align(Alignment.BottomEnd))
+                }
+
+//                Column(
+//                    modifier = Modifier.fillMaxSize(),
+//                    verticalArrangement = Arrangement.spacedBy(10.dp),
+//                    horizontalAlignment = Alignment.CenterHorizontally
+//                ) {
+//                    Text(text = "Hello")
+//                    Text(text = "World")
+//                    Text(text = "I'm")
+//                    Text(text = "jieun")
+//                }
+
+//                Row(
+//                    modifier = Modifier.fillMaxSize(),
+//                    horizontalArrangement = Arrangement.SpaceEvenly,
+//                    verticalAlignment = Alignment.CenterVertically
+//                ) {
+//                    Text(text = "Hello")
+//                    Text(text = "World")
+//                    Text(text = "I'm")
+//                    Text(text = "jieun")
+//                }
+            }
+
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Welcome $name", color = Color.Blue, fontSize = 22.sp, fontWeight = FontWeight.Bold)
 }
